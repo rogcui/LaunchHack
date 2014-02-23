d391a71809de86e6f0967bbe2a50c733c4b611ac
dserrano3 <dserrano3@hotmail.com>
2014-02-22 16:44:30 -0800

    daniel

--- src/com/example/launchhack/MainActivity.java
+++ src/com/example/launchhack/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+    //comment to test (daniel)
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
